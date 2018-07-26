cc4021fa95956355500c8bdeb0299096e83d6dcd
SreyaVangara <svangara@gmail.com>
2018-07-25 22:52:09 -0400

    Created parser to properly format sources text file

--- python-scripts/online-data-reader.py
+++ python-scripts/online-data-reader.py
@@ -1,22 +1,42 @@
 import urllib.request
+import re
+
+sources = open('sources.txt', 'r')
+info = sources.read()
+#print(info)
+
+
+output = open('BRO_formatted.txt','w')
+
 
 url="http://rules.emergingthreats.net/open/suricata/rules/compromised-ips.txt"
+src = "et_ips"
 
-##wp = urllib.request.urlopen(url)
-##pw = wp.read()
-##pw=str(pw)
-##a = "safsdafs\nsdfa"
-###npw=pw.split()
-##print(pw)
+raw_data = urllib.request.urlopen(url)
+
+data = list ( set ( raw_data.read().decode('utf-8').splitlines() ) )
+
+#meta_desc= desc(data[0])
+meta_desc="IP"
+
+for intel in data:
+    line=[]
+    line.append(intel)
+    
+    if re.search('[a-zA-Z]', intel):
+        line.append("Intel::DOMAIN")
+    else:
+        line.append("Intel::ADDR")
+    line.append(src)
+    line.append(meta_desc)
+    line.append(url)
+    
+    output.write("  ".join(line)+'\n')
+
+output.close()
 
 
-data = urllib.request.urlopen(url)
-
-d = data.read().decode('utf-8').splitlines()
-
-print(d)
 
 
 
-#for line in data: # files are iterable
-#    print (line)
+
