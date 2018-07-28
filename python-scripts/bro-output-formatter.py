f0144bb71fe61ebbd12aa3e73e80d7442a3855a5
SreyaVangara <svangara@gmail.com>
2018-07-27 22:22:24 -0400

    Non-download data collection done

---
+++ python-scripts/bro-output-formatter.py
@@ -0,0 +1,55 @@
+import urllib.request
+import re
+
+
+intel_type = {'IP' : '::ADDR' , 'DOMAINS' : '::DOMAIN' , 'URLS' : 'URL' , 'SHA-1' : '::CERT_HASH'}
+
+
+raw_intel = open('formatted-intel.txt', 'r')
+#total_intel = raw_intel.read().splitlines()
+
+
+output = open('bro-intel.txt','w')
+
+for intel in raw_intel:
+    #print(intel)
+    #print(source)
+    details = intel.split()
+    #print(details)
+    meta_src = details[0]
+
+##        raw_data = urllib.request.urlopen(url)
+##
+##        data = list ( set ( raw_data.read().decode('utf-8').splitlines() ) )
+
+    #meta_desc= desc(data[0])
+    meta_desc=details[4]
+    field = details[3]
+    meta_url=details[1]
+    indicator = intel_type[details[2].upper()]
+    line = [field, indicator, meta_src, meta_desc, meta_url]
+    #print(line)
+
+##        for intel in data:
+##            line=[]
+##            line.append(intel)
+##            
+##            if re.search('[a-zA-Z]', intel):
+##                line.append("Intel::DOMAIN")
+##            else:
+##                line.append("Intel::ADDR")
+##            line.append(src)
+##            line.append(meta_desc)
+##            line.append(url)
+        
+    output.write("  ".join(line)+'\n')
+        
+
+
+output.close()
+
+
+
+
+
+
--- python-scripts/intel-file-creator.py
+++ python-scripts/intel-file-creator.py
@@ -5,35 +5,27 @@ import urllib.request
 sources = open('sources.txt', 'r')
 info = sources.read().splitlines()
 #print(info)
-output = open('BRO_formatted.txt','w')
+output = open('formatted-intel.txt','w')
 
 for source in info:
     source=source.split()
-    print(source[1])
-    print()
     if source[0].upper() in ['SNORT', 'TALOS', 'ET_IPS']:
         raw_data = urllib.request.urlopen(source[1])
 
         data = list ( set ( raw_data.read().decode('utf-8').splitlines() ) )
-        print (data)
+        for r in data:
+            output.write('    '.join(source) + '    '+r+ '    ' + '-' + '\n')
     
     elif source[0] == 'abuse':
-        raw_data = urllib.request.urlopen(source[1], 'rt')
-        reader = csv.reader(raw_data)
-        data= [ r for r in reader]
-        data.pop(0)
-        print(data)
+        raw_data = urllib.request.urlopen('https://sslbl.abuse.ch/blacklist/sslblacklist.csv')
+
+        data = raw_data.read().decode('utf-8').splitlines()
+        for r in data:
+            if r[0]!='#':
+                output.write('    '.join(source) + '    ' + r[r.find(',')+1: ].replace(',','    ') + '\n')
+
+output.close()
 
 
-##    try:
-##        #print(source)
-##        details = source.split('    ')
-##        #print(details)
-##        url=details[1]
-##        src = details[0]
-##
-##        raw_data = urllib.request.urlopen(url)
-##
-##        data = list ( set ( raw_data.read().decode('utf-8').splitlines() ) )
 
 
